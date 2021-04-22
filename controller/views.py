0022054e9a743c94e51aab90b9760c148f2128c3
twilight2017 <41724039@xs.ustb.edu.cn>
2021-04-22 09:17:54 +0800

    fourth commit

--- controller/views.py
+++ controller/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from .models import Tests
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.db import transaction
+import os
+from django.shortcuts import HttpResponse
+import xlwt
+from io import BytesIO
+import qrcode
+import xlrd
 # Create your views here.
+
+def test(request):
+    if request.method == "POST" and request.POST:
+        s_id = request.POST.get('source_id')
+        t_id = request.POST.get('target_id')
+        # 创建新的测试对象，存入数据库中
+        info = Tests()
+        info.source_id = s_id
+        info.target_id = t_id
+        info.save()
+        return render(request, 'home.html')
+    return render(request, 'home.html')
--- generation/urls.py
+++ generation/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from controller import views
+from django.views.static import serve
+from generation.settings import FILES_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test, name="SIGNAL")
 ]
