4f0c1f770244df5ed479decf5a04e5ce727c8d14
twilight2017 <38035081+twilight2017@users.noreply.github.com>
2021-05-10 17:58:55 +0800

    Add files via upload

--- controller/views.py
+++ controller/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.db import transaction
 import os
+import charts
+import echarts
 import subprocess
+import webbrowser
 from subprocess import Popen, PIPE
 from django.shortcuts import HttpResponse
 import xlwt
@@ -13,10 +16,6 @@ from io import BytesIO
 import qrcode
 import xlrd
 # Create your views here.
-global cout
-
-def test(request):
-    return render(request, 'home.html')
 
 def algorithm(request):
     if request.method == "POST" and request.POST:
@@ -51,3 +50,44 @@ def algorithm(request):
         display = Tests.objects.get(target_acc=dit[2])
         return render(request, 'identify.html',{'test_list':display})
     return render(request,'identify.html')
+
+def line(request):
+    print('line_success')
+    # 获取当前数据库中所有tests()对象
+    dis = Tests.objects.filter()
+    LIST = []
+    count = 0  # count记录当前的迭代总次数
+    for i in dis:
+        LIST.append(i)
+        count += 1
+    # 获取所有的源数据集准确率对象
+    s_acc = []
+    for li in LIST:
+        s_acc.append(li.source_acc)
+    # 获取当前所有的目标数据集准确率对象
+    t_acc = []
+    for lin in LIST:
+        t_acc.append(lin.target_acc)
+    charts.paint(count, s_acc, t_acc)
+    webbrowser.open('D:\study\graduation_project\grdaution_project\\templates\line.html')
+    return render(request,'identify.html')
+
+def bar(request):
+    # 获取当前数据库中所有tests()对象
+    dis = Tests.objects.filter()
+    LIST = []
+    count = 0  # count记录当前的迭代总次数
+    for i in dis:
+        LIST.append(i)
+        count += 1
+    # 获取所有的源数据集训练时长对象
+    s_time = []
+    for li in LIST:
+        s_time.append(li.source_time)
+    # 获取当前所有的目标数据集训练时长对象
+    t_time = []
+    for lin in LIST:
+        t_time.append(lin.target_time)
+    echarts.paint(count, s_time, t_time)
+    webbrowser.open('D:\study\graduation_project\grdaution_project\\templates\\bar.html')
+    return render(request, 'identify.html')
